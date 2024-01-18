2f6213ce56a162ed1e2af0b63052f3b7cb886dc4
igormanoels <s.igormanoel@gmail.com>
2024-01-17 21:02:37 -0300

    Primeiro Contato com C Sharp

---
+++ HelloWord/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            //Estou livre da maldição!
+            Console.ReadLine();
+        }
+    }
+}
